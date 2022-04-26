00867fbb99320beb93e3b50e31f183a17410a129
stefanylimas <stefany_limaa@hotmail.com>
2022-04-26 15:58:42 -0300

    first commit

---
+++ js/admin.js
@@ -0,0 +1,56 @@
+const tableBodySelector = document.querySelector('#table-body');
+
+function createElementFromEvent(data) {
+    data.forEach((event, index) => {
+        const trElement = document.createElement('tr');
+
+        const thElement = document.createElement('th');
+        thElement.setAttribute('scope', 'row')
+        thElement.innerText = index + 1;
+
+        const firstTdElement = document.createElement('td');
+        const date = event.scheduled.substring(0, 10).replaceAll('-', '/');
+        const time = event.scheduled.substring(11, 16)
+        firstTdElement.innerText = date + " " + time;
+
+        const secondTdElement = document.createElement('td');
+        secondTdElement.innerText = event.name;
+
+        const thirdTdElement = document.createElement('td');
+        thirdTdElement.innerText = event.attractions.join(', ');
+
+        const fourthTdElement = document.createElement('td');
+
+        const firstAnchor = document.createElement('a');
+        firstAnchor.innerText = "ver reservas"
+        firstAnchor.classList.add('btn')
+        firstAnchor.classList.add('btn-dark')
+
+        const secondAnchor = document.createElement('a');
+        secondAnchor.innerText = "editar"
+        secondAnchor.href = "editar-evento.html?id=" + event._id;
+        secondAnchor.classList.add('btn')
+        secondAnchor.classList.add('btn-secondary')
+
+        const thirdAnchor = document.createElement('a');
+        thirdAnchor.innerText = "excluir"
+        thirdAnchor.href = "excluir-evento.html?id=" + event._id;
+        thirdAnchor.classList.add('btn')
+        thirdAnchor.classList.add('btn-danger')
+
+
+
+        fourthTdElement.append(firstAnchor, secondAnchor, thirdAnchor);
+        tableBodySelector.appendChild(trElement);
+        trElement.append(thElement, firstTdElement, secondTdElement, thirdTdElement, fourthTdElement);
+
+
+    });
+}
+
+fetch("https://xp41-soundgarden-api.herokuapp.com/events", {
+        "method": "GET"
+    }).then(response => {
+        return response.json()
+    }).then(data => createElementFromEvent(data))
+    .catch(error => console.log(error));
